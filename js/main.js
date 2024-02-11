54165d1bdbb0e1288a3c0148e5e929f5a9698f3e
Tisotti <yana.albertivna@gmail.com>
2024-02-11 22:38:41 +0100

    правки

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(function () {
-    
-    
+  var mixer = mixitup('.work__gallery');
+
+  $('.work__link').on('click', function (event) {
+    event.preventDefault();
+    var filterValue = $(this).attr('data-filter');
+    mixer.filter(filterValue);
+  });
+
 });
+
